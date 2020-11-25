cc99875c02b67e0d398302ecf98102f6a9b61a94
diegogf10 <diego.garcia.fierro@alumnos.upm.es>
2020-11-25 22:01:34 +0100

    <Start>, <Stop> and <Release> orders V2 partially tested

--- pc1.py
+++ pc1.py
@@ -7,39 +7,46 @@ from subprocess import call
 import sys
 import logging
 import configparser
+import os 
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('pc1')
 
 config = configparser.ConfigParser()
 
+def setUpConfigFile( nServers, nClients, nLB ):
+    config['VARIABLES'] = {'num_serv' : nServers,
+                           'num_client' : nClients,
+                           'num_lb': nLB}
+    with open('pc1.cfg', 'w') as configfile:
+        config.write(configfile)
+
+
 #Find number of servers 
 def findNServ():
-    call(["sudo", "cd", ""])
-    call(["sudo", "cd", "/CDPS/PCreativaCDPS"])
-    num_serv = config['pc1.cfg']['num_serv']
+    config.read('pc1.cfg')
+    num_serv = config['VARIABLES']['num_serv']
     return num_serv
 
 #Find number of clients
 def findNCli():
-    call(["sudo", "cd", ""])
-    call(["sudo", "cd", "/CDPS/PCreativaCDPS"])
-    num_client = config['pc1.cfg']['num_client']
+    config.read('pc1.cfg')
+    num_client = config['VARIABLES']['num_client']
     return num_client
 
 #Find number of load balancers 
 def findNLoadBalanc():
-    call(["sudo", "cd", ""])
-    call(["sudo", "cd", "/CDPS/PCreativaCDPS"])
-    num_lb = config['pc1.cfg']['num_lb']
+    config.read('pc1.cfg')
+    num_lb = config['VARIABLES']['num_lb']
     return num_lb
 
 #Set configuration for either the start, the stop or the release order
 def setConfig(order):
+    setUpConfigFile(3, 1, 1)
     num = [findNServ(), findNCli(), findNLoadBalanc()]
     words = ["s", "c", "lb"]
     for m,n in zip(num, words):
-        for i in range (0,m+1):
+        for i in range (0,int(m)):
             call (["sudo", "virsh", f"{order}", f"{n}{i}"])
             
 #Open virtual machine consoles
@@ -47,31 +54,24 @@ def openConsoles():
     num = [findNServ(), findNCli(), findNLoadBalanc()]
     words = ["s", "c", "lb"]
     for m,n in zip(num, words):
-        for i in range (0,m+1): 
-            call(["nohup","xterm", "-e", """ "sudo """, "virsh", "console", f""" {n}{i}" """, "&"])
+        for i in range (0,int(m)): 
+            os.system(f"xterm -e \'sudo virsh console {n}{i}\'&")
         
 #Start virtual machines and show their consoles
 def startOrder():
-    call(["sudo", "cd", ""])
-    call(["sudo", "cd", "/CDPS/PCreativaCDPS"])
     call(["sudo", "virt-manager"])
     setConfig("start")
-    
+    openConsoles()
 
 #Stop virtual machines saving their current state
 def stopOrder():
-    call(["sudo", "cd", ""])
-    call(["sudo", "cd", "/CDPS/PCreativaCDPS"])
     call(["sudo", "virt-manager"])
-    setConfig("stop")
+    setConfig("shutdown")
 
 #Delete all created files releasing the practice's scenario
 def releaseOrder():
-    call(["sudo", "cd", ""])
-    call(["sudo", "cd", "/CDPS/PCreativaCDPS"]) 
     call(["sudo", "virt-manager"])
-    setConfig("release")
-    openConsoles()
+    setConfig("destroy")
     
     
 #if sys.argv[1] == "create":
